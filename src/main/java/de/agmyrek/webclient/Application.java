47915f637b8bfb00f10d8c314743ae784e35e2cc
Andreas Gmyrek <a.gmyrek@thalia.de>
2021-02-19 22:06:45 +0100

    Client konfiguriert

---
+++ src/main/java/de/agmyrek/webclient/Application.java
@@ -0,0 +1,16 @@
+package de.agmyrek.webclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+@EnableConfigurationProperties
+public class Application extends SpringBootServletInitializer {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
